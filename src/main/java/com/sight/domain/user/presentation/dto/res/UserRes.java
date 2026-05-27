73883c0ed42f471ce11110f9acfbdf093bccd026
Ljh081021 <loveljh081021@dgsw.hs.kr>
2026-05-27 16:09:23 +0900

    fix: user response

--- src/main/java/com/sight/domain/user/presentation/dto/res/UserRes.java
+++ src/main/java/com/sight/domain/user/presentation/dto/res/UserRes.java
@@ -1,21 +1,29 @@
 package com.sight.domain.user.presentation.dto.res;
 
 import com.sight.domain.user.domain.User;
+import jakarta.persistence.ElementCollection;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record UserRes(
+        Long id,
         String name,
-        String password,
         String profile,
-        LocalDateTime created_at
+        LocalDateTime created_at,
+        List<Long> liked,
+        List<Long> disliked,
+        List<Long> bookmarked
 ) {
     public static UserRes from(User user) {
         return new UserRes(
+                user.getId(),
                 user.getName(),
-                user.getPassword(),
                 user.getProfile(),
-                user.getCreated_at()
+                user.getCreated_at(),
+                user.getLiked(),
+                user.getDisliked(),
+                user.getBookmarked()
         );
     }
 }
